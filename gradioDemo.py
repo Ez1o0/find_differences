ec994b272a14b96997ef92e2debe5f88beab6be2
ez1o0 <411152084@qq.com>
2024-08-05 17:39:45 +0800

    增加图片过大的判断

--- gradioDemo.py
+++ gradioDemo.py
@@ -1,11 +1,38 @@
 import gradio as gr
 import cv2
 import numpy as np
+# from PIL import Image
+# import warnings
 
 def find_differences(image1, image2):
+    # Calculate the number of pixels
+    num_pixels1 = image1.shape[0] * image1.shape[1]
+    num_pixels2 = image2.shape[0] * image2.shape[1]
+
+    # Check if the number of pixels exceeds your safe threshold
+    if num_pixels1 > 89478485 or num_pixels2 > 89478485:
+        gr.Warning("图片过大，将进行压缩")
+
+        # 等比例压缩图片
+        scale_factor1 = (89478485 / num_pixels1) ** 0.5
+        width1 = int(image1.shape[1] * scale_factor1)
+        height1 = int(image1.shape[0] * scale_factor1)
+        # 使用 INTER_AREA 方法进行高质量压缩
+        image1 = cv2.resize(image1, (width1, height1), interpolation=cv2.INTER_AREA)
+
+        # 等比例压缩图片
+        scale_factor2 = (89478485 / num_pixels2) ** 0.5
+        width2 = int(image2.shape[1] * scale_factor2)
+        height2 = int(image2.shape[0] * scale_factor2)
+        # 使用 INTER_AREA 方法进行高质量压缩
+        image2 = cv2.resize(image2, (width2, height2), interpolation=cv2.INTER_AREA)
+
+        if image1.shape[0] * image1.shape[1] > 89478485 or image2.shape[0] * image2.shape[1] > 89478485:
+            raise gr.Error("图片过大，压缩后仍然超过限制")
+
     # 转换为灰度图像
-    gray1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
-    gray2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
+    gray1 = cv2.cvtColor(image1, cv2.COLOR_RGB2GRAY)
+    gray2 = cv2.cvtColor(image2, cv2.COLOR_RGB2GRAY)
 
     # 计算差异并阈值化
     difference = cv2.absdiff(gray1, gray2)
@@ -39,7 +66,7 @@ def find_differences(image1, image2):
 # 创建Gradio接口
 app = gr.Interface(
     fn=find_differences,
-    inputs=[gr.Image(label="上传图片1", image_mode='RGB'), gr.Image(label="上传图片2", image_mode='RGB')],
+    inputs=[gr.Image(label="上传图片1", image_mode='RGB', type='numpy'), gr.Image(label="上传图片2", image_mode='RGB', type='numpy')],
     outputs=gr.Image(label="处理后的图片"),
     title="查找两种图片的不同",
     description="上传两张图片",
